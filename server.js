b412ddf754685f4e7f6acbfedf1b19392b85176a
Tamara Brkic <brkictamara7@gmail.com>
2022-12-21 18:32:38 +0100

    backend auth

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors')
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const PORT = process.env.PORT || 5000;
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the Crypto Diary - Code by Comtrade final project' });
+});
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
+// app.use('/api/tickets', require('./routes/ticketRoutes'));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+module.exports = app;
